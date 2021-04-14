88d37582821cf083c4aae1dd072c54735e786bce
akaregi <verdigris@azure.icu>
2021-04-14 20:27:27 +0900

    This is suited for GitHub Pages(TM)
    Signed-off-by: akaregi <verdigris@azure.icu>

--- gulpfile.ts
+++ gulpfile.ts
@@ -7,23 +7,24 @@ import { promises as fs } from 'fs'
 // TODO: d.ts を作るべきか？ しかし手間。
 const markdown: any = require('gulp-markdown')
 
+// FIXME: UNSTABLE! WHAT THE ASYNC F---
 function build(done: () => any) {
     (async () => {
-        src('docs/*.md')
+        src('markdown/*.md')
             .pipe(concat('all.md'))
-            .pipe(dest('./dist/'))
+            .pipe(dest('./docs/'))
             .pipe(markdown({
                 breaks: true,
                 smartypants: true
             }))
-            .pipe(dest('./dist/'))
+            .pipe(dest('./docs/'))
 
-        const view = (await fs.readFile('./views/index.html')).toString()
-        const contents = (await fs.readFile('./dist/all.html')).toString()
+        const view = await fs.readFile('./views/index.html')
+        const contents = await fs.readFile('./docs/all.html')
 
-        const final = view.replace('%DATA%', contents)
+        const final = view.toString().replace('%DATA%', contents.toString())
 
-        await fs.writeFile('dist/index.html', final);
+        await fs.writeFile('docs/index.html', final);
     })();
 
     done()
